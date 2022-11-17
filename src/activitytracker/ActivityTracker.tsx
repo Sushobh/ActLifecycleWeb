daae7686eb3a066e4ee0631ed465a54b7b9e4d61
Sushobh <aryansushobh@gmail.com>
2022-11-17 11:53:38 +0530

    Extracted interface and other refactoring

--- src/activitytracker/ActivityTracker.tsx
+++ src/activitytracker/ActivityTracker.tsx
@@ -8,68 +8,100 @@ import { useState } from 'react';
 import { TransitionGroup, CSSTransition } from "react-transition-group"
 import * as React from 'react';
 import { useTransition, animated } from '@react-spring/web'
+import { makeRandomString } from '../Utils';
 
-export class AndroidEvent {
-    eventType: string = ""
-    eventData: any = {}
+
+const MAX_EVENTS_TO_DISPLAY = 100
+const POLLING_INTERVAL = 2000
+
+
+export interface IAndroidEvent {
+    eventName : string 
+    data : string 
+    id : string 
+}
+export class AndroidEvent implements IAndroidEvent {
+    eventName: string = ""
+    data: string = ""
     id: string = ''
-    constructor(id: string, eventType: string, eventData: any) {
-        this.eventType = eventType
-        this.eventData = eventData
+    constructor(id: string, eventType: string, eventData: string) {
+        this.eventName = eventType
+        this.data = eventData
         this.id = id
     }
 }
 
 function VWEventItem(props: any) {
-    let data: AndroidEvent = props.info
+    let data: IAndroidEvent = props.info
+    console.log("Rendering")
+    console.log(data)
     return (
         <ListItem className={styles.listItem}>
             <div className={styles.eventFullContainer}>
-                <Card className={styles.tagCard} style={{ "backgroundColor": "lavender" }}><p >{data.eventType}</p></Card>
+                <Card className={styles.tagCard} style={{ "backgroundColor": "lavender" }}><p >{data.eventName}</p></Card>
                 <Card className={styles.card} style={{ "backgroundColor": "cadetblue" }}>
-                    <p>{data.eventData.text}</p>
+                    <p>{data.data}</p>
                 </Card>
             </div>
         </ListItem>)
+}
+
+export function getEvents() {
+    return fetch('http://localhost:4200/getalllifecycleevents/')
+        .then(data => {
+            return data.json().then(newData => JSON.parse(newData))
+        }).then(data => {
+            return data.map((event: any) => {
+                let androidEvent : IAndroidEvent = event
+                androidEvent.id = makeRandomString(10)
+                return event;
+            })
+        }).catch(err => {
+            return new Promise((resolveInner) => {
+                resolveInner([new AndroidEvent("384234","Error","Something has gone wrong, activity tracker not able to connect with api.")]) 
+              })
+        })
 }
 
 
 
 export function VWEventListing() {
     {
-        let [data, setData] = useState(new Array<AndroidEvent>())
+        let [data, setData] = useState(new Array<IAndroidEvent>())
         useInterval(() => {
-            let event = new AndroidEvent(data.length + 1 + '', 'Activity', { text: 'MainActivity => OnCreate'+(data.length+1) })
-            let event1 = new AndroidEvent(data.length + 2 + '', 'Activity', { text: 'MainActivity => OnCreate'+(data.length+2) })
-            let event2 = new AndroidEvent(data.length + 3 + '', 'Activity', { text: 'MainActivity => OnCreate'+(data.length+3) })
-            addNewItems([event])
-        }, 2000);
+               getEvents().then(data => {
+                addNewItems(data)
+               })
+        }, POLLING_INTERVAL);
 
 
- 
-
-        let addNewItems = (newItems : Array<AndroidEvent>) => {
+        let addNewItems = (newItems: Array<IAndroidEvent>) => {
+            if(data.length >= MAX_EVENTS_TO_DISPLAY){
+                for(var i = 0;i<Math.min(newItems.length,data.length);i++){
+                    data.pop()
+                }
+            }
             setData([...newItems, ...data])
         }
 
-
         const transitions = useTransition(data, {
             from: item => {
-                return { height : '0px', opacity : '0', transform: 'scaleX(0)' }
+                return { height: '0px', opacity: '0', transform: 'scaleX(0)' }
             },
             enter: item => {
-                return { height : '60px' , opacity : '1', transform: 'scaleX(1)'}
-            },
+                return { height: '60px', opacity: '1', transform: 'scaleX(1)' }
+            }
         })
-
+       
         return (
             <div className={styles.maincontainer}>
                 <h3>Activity/Fragment lifecycle tracker</h3>
+                <h5>Polling Interval = {POLLING_INTERVAL}, Currently dispalyed event count = {data.length}</h5>
                 <List className={styles.list}>
                     {
                         transitions((style, item) => (
                             <animated.div style={style} key={item.id}>
-                                <VWEventItem  info={item}></VWEventItem>
+                                <VWEventItem info={item}></VWEventItem>
                             </animated.div>
                         ))
                     }
