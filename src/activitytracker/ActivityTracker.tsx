7c3c02f6b8469e8f44157b8901e84b7f701f979e
Sushobh <aryansushobh@gmail.com>
2022-11-19 11:50:01 +0530

    Updated

--- src/activitytracker/ActivityTracker.tsx
+++ src/activitytracker/ActivityTracker.tsx
@@ -11,35 +11,64 @@ import { useTransition, animated } from '@react-spring/web'
 import { makeRandomString } from '../Utils';
 
 
-const MAX_EVENTS_TO_DISPLAY = 100
-const POLLING_INTERVAL = 2000
+const MAX_EVENTS_TO_DISPLAY = 50
+const POLLING_INTERVAL = 500
 
 
 export interface IAndroidEvent {
     eventName : string 
     data : string 
-    id : string 
+    id : string
+    style : any
+
+    isErrorEvent() : boolean
 }
-export class AndroidEvent implements IAndroidEvent {
-    eventName: string = ""
-    data: string = ""
-    id: string = ''
-    constructor(id: string, eventType: string, eventData: string) {
-        this.eventName = eventType
-        this.data = eventData
-        this.id = id
+
+
+
+export class ErrorEvent implements IAndroidEvent {
+    eventName: string = "";
+    data: string = "Unable to connect with API :(";
+    id: string = "";
+    style : any = {}
+    isErrorEvent(): boolean {
+        return true
     }
+}
+
+
+
+export class AndroidEvent implements IAndroidEvent {
+    eventName: string 
+    data: string 
+    id: string 
+    style: any
+    event : IAndroidEvent
+    constructor(event : IAndroidEvent){
+        this.event = event
+        this.eventName = this.event.eventName;
+        this.data = this.event.data;
+        this.id = this.event.id;
+        this.style = this.event.style;
+    }
+    isErrorEvent(): boolean {
+        return false
+    }
+
 }
 
 function VWEventItem(props: any) {
     let data: IAndroidEvent = props.info
-    console.log("Rendering")
-    console.log(data)
+    if(data.isErrorEvent()){
+        return (<ListItem>
+                   <ListItemText>Unable to connect with Api :(</ListItemText>
+        </ListItem>)
+    }
     return (
         <ListItem className={styles.listItem}>
             <div className={styles.eventFullContainer}>
-                <Card className={styles.tagCard} style={{ "backgroundColor": "lavender" }}><p >{data.eventName}</p></Card>
-                <Card className={styles.card} style={{ "backgroundColor": "cadetblue" }}>
+                <Card className={styles.tagCard} style={data.style.tagStyle ? data.style.tagStyle : {}}><p >{data.eventName}</p></Card>
+                <Card className={styles.card} style={data.style.cardStyle ? data.style.cardStyle : {}}>
                     <p>{data.data}</p>
                 </Card>
             </div>
@@ -47,18 +76,18 @@ function VWEventItem(props: any) {
 }
 
 export function getEvents() {
-    return fetch('http://localhost:4200/getalllifecycleevents/')
+    return fetch('/getlifecycleevents/')
         .then(data => {
             return data.json().then(newData => JSON.parse(newData))
         }).then(data => {
+            
             return data.map((event: any) => {
                 let androidEvent : IAndroidEvent = event
-                androidEvent.id = makeRandomString(10)
-                return event;
+                return new AndroidEvent(androidEvent);
             })
         }).catch(err => {
             return new Promise((resolveInner) => {
-                resolveInner([new AndroidEvent("384234","Error","Something has gone wrong, activity tracker not able to connect with api.")]) 
+                resolveInner([new ErrorEvent()]) 
               })
         })
 }
@@ -76,8 +105,13 @@ export function VWEventListing() {
 
 
         let addNewItems = (newItems: Array<IAndroidEvent>) => {
-            if(data.length >= MAX_EVENTS_TO_DISPLAY){
-                for(var i = 0;i<Math.min(newItems.length,data.length);i++){
+            if(data.length > 0 && data[0].isErrorEvent() && newItems.length > 0 && newItems[0].isErrorEvent()){
+                //Already displaying an error message, no need to add a new row of error message
+                return
+            }
+            if(data.length + newItems.length > MAX_EVENTS_TO_DISPLAY){
+                let numOfItemsToRemove = data.length+newItems.length - MAX_EVENTS_TO_DISPLAY
+                for(var i = 0;i<Math.min(numOfItemsToRemove,data.length);i++){
                     data.pop()
                 }
             }
@@ -92,11 +126,11 @@ export function VWEventListing() {
                 return { height: '60px', opacity: '1', transform: 'scaleX(1)' }
             }
         })
-       
+        let pollingIntervalText = (POLLING_INTERVAL/1000)+ ' seconds' 
         return (
             <div className={styles.maincontainer}>
                 <h3>Activity/Fragment lifecycle tracker</h3>
-                <h5>Polling Interval = {POLLING_INTERVAL}, Currently dispalyed event count = {data.length}</h5>
+                <h5>Polling Interval = {pollingIntervalText}, Currently dispalyed event count = {data.length} , event limit = {MAX_EVENTS_TO_DISPLAY}</h5>
                 <List className={styles.list}>
                     {
                         transitions((style, item) => (
